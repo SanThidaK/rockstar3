6fd24496779b0c2856892b9cd993e2d8ab8abba9
zawzaw572001 <zawzaw572001@gmail.com>
2019-06-09 17:27:57 +0630

    Create Todo project using react

---
+++ hello-react/src/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Item from './Item';
+
+class List extends React.Component
+{
+    render()
+    {
+        return(
+            <ul>
+                {
+                    this.props.data.map( item => {
+                        return(
+                            <Item name={ item } />
+                        )
+                    } )
+                }
+            </ul>
+        )
+    }
+}
+
+export default List;
